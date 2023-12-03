a7ab8badbe4ca044fbb08266662ef4a6f7d74d27
SusBin <sb2096@hw.ac.uk>
2023-12-03 21:14:14 +0000

    Stacks & recusion added

--- Services/QuestionService.cs
+++ Services/QuestionService.cs
@@ -60,6 +60,7 @@ namespace Revise.Services
             return course?.Topics.FirstOrDefault(t => t.Topic == topicName);
         }
 
+        
         public void UpdateQuestion(int courseId, string topicName, RevisionQuestion updatedQuestion)
         {
             var course = _courses.FirstOrDefault(c => c.Id == courseId);
@@ -68,12 +69,16 @@ namespace Revise.Services
                 var topic = course.Topics.FirstOrDefault(t => t.Topic == topicName);
                 if (topic != null)
                 {
-                    var index = topic.Questions.FindIndex(q => q.Id == updatedQuestion.Id);
-                    if (index != -1)
+                    var question = topic.Questions.FirstOrDefault(q => q.Id == updatedQuestion.Id);
+                    if (question != null)
                     {
-                        topic.Questions[index] = updatedQuestion;
-                        var newJson = JsonSerializer.Serialize(_courses);
-                        System.IO.File.WriteAllText("questions.json", newJson);
+                        var index = topic.Questions.FindIndex(q => q.Id == updatedQuestion.Id);
+                        if (index != -1)
+                        {
+                            topic.Questions[index] = updatedQuestion;
+                            var newJson = JsonSerializer.Serialize(_courses);
+                            System.IO.File.WriteAllText("questions.json", newJson);
+                        }
                     }
                 }
             }
@@ -95,12 +100,45 @@ namespace Revise.Services
                 }
             }
         }
+        public int GetNextQuestionId(int courseId)
+        {
+            // Get the course by Id
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+
+            // Check if the course exists
+            if (course == null)
+            {
+                throw new Exception("Course not found.");
+            }
+
+            // Get all questions for the course
+            var questions = course.Topics.SelectMany(t => t.Questions).ToList();
+
+            // Check if there are any questions
+            if (!questions.Any())
+            {
+                throw new Exception("No questions available for this course.");
+            }
+
+            // Get the last question
+            var lastQuestion = questions.Last();
+
+            // Assuming the questions have an Id property
+            var lastQuestionId = lastQuestion.Id;
+
+            // Get the next question Id
+            var nextQuestionId = lastQuestionId + 1;
+
+            return nextQuestionId;
+        }
 
         public void AddNewQuestion(int courseId, string topicName, RevisionQuestion newQuestion)
         {
             var course = GetCourseById(courseId);
             if (course != null)
             {
+                // Set the Id of the new question
+                newQuestion.Id = GetNextQuestionId(courseId);
                 var topic = course.Topics.FirstOrDefault(t => t.Topic == topicName);
                 if (topic != null)
                 {
@@ -108,6 +146,125 @@ namespace Revise.Services
                     topic.Questions.Add(newQuestion);
                     var newJson = JsonSerializer.Serialize(_courses);
                     System.IO.File.WriteAllText("questions.json", newJson);
+                }
+            }
+        }
+
+        public RevisionQuestion GetRandomQuestion(int courseId)
+        {
+            // Get the course by Id
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+
+            // Check if the course exists
+            if (course == null)
+            {
+                throw new Exception("Course not found.");
+            }
+
+            // Get all questions for the course
+            var questions = course.Topics.SelectMany(t => t.Questions).ToList();
+
+            // Check if there are any questions
+            if (!questions.Any())
+            {
+                throw new Exception("No questions available for this course.");
+            }
+
+            // Use System.Random to generate a random index
+            var random = new System.Random();
+            var randomIndex = random.Next(questions.Count);
+
+            // Return the randomly selected question
+            return questions[randomIndex];
+        }
+        
+        public void DeleteQuestion(int courseId, string topicName, int id)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+            if (course != null)
+            {
+                var topic = course.Topics.FirstOrDefault(t => t.Topic == topicName);
+                if (topic != null)
+                {
+                    var question = topic.Questions.FirstOrDefault(q => q.Id == id);
+                    if (question != null)
+                    {
+                        topic.Questions.Remove(question);
+                        var newJson = JsonSerializer.Serialize(_courses);
+                        System.IO.File.WriteAllText("questions.json", newJson);
+                    }
+                }
+            }
+        }
+
+        public void DeleteTopic(int courseId, string topicName)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+            if (course != null)
+            {
+                var topic = course.Topics.FirstOrDefault(t => t.Topic == topicName);
+                if (topic != null)
+                {
+                    course.Topics.Remove(topic);
+                    var newJson = JsonSerializer.Serialize(_courses);
+                    System.IO.File.WriteAllText("questions.json", newJson);
+                }
+            }
+        }
+
+        public void DeleteCourse(int courseId)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+            if (course != null)
+            {
+                _courses.Remove(course);
+                var newJson = JsonSerializer.Serialize(_courses);
+                System.IO.File.WriteAllText("questions.json", newJson);
+            }
+        }
+
+
+        public void AddNewCourse(Course newCourse)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == newCourse.Id);
+            if (course == null)
+            {
+                _courses.Add(newCourse);
+                var newJson = JsonSerializer.Serialize(_courses);
+                System.IO.File.WriteAllText("questions.json", newJson);
+            }
+        }
+
+        public void UpdateCourse(Course updatedCourse)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == updatedCourse.Id);
+            if (course != null)
+            {
+                var index = _courses.FindIndex(c => c.Id == updatedCourse.Id);
+                if (index != -1)
+                {
+                    _courses[index] = updatedCourse;
+                    var newJson = JsonSerializer.Serialize(_courses);
+                    System.IO.File.WriteAllText("questions.json", newJson);
+                }
+            }
+        }
+
+        public void UpdateTopic(int courseId, string topicName, StudyTopic updatedTopic)
+        {
+            var course = _courses.FirstOrDefault(c => c.Id == courseId);
+            if (course != null)
+            {
+                var topic = course.Topics.FirstOrDefault(t => t.Topic == topicName);
+                if (topic != null)
+                {
+                    var index = course.Topics.FindIndex(t => t.Topic == topicName);
+                    if (index != -1)
+                    {
+                        course.Topics[index] = updatedTopic;
+                        var newJson = JsonSerializer.Serialize(_courses);
+                        System.IO.File.WriteAllText("questions.json", newJson);
+                    }
                 }
             }
         }
